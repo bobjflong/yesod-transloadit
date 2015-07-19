fce85f767d87f2a87c428e80b0c923ff2eb04294
Bob Long <robertjflong@gmail.com>
2015-07-19 16:54:15 +0100

    bump major version

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ let
       }:
       mkDerivation {
         pname = "yesod-transloadit";
-        version = "0.2.1.0";
+        version = "0.3.0.0";
         src = ./.;
         buildDepends = [
           aeson base byteable bytestring cryptohash lens lens-aeson
