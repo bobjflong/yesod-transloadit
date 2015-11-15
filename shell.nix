475961523485bb55da128c22629d941ef90eabcf
Bob Long <robertjflong@gmail.com>
2015-11-15 23:39:23 +0000

    bump version

--- shell.nix
+++ shell.nix
@@ -11,7 +11,7 @@ let
       }:
       mkDerivation {
         pname = "yesod-transloadit";
-        version = "0.5.0.0";
+        version = "0.6.0.0";
         src = ./.;
         libraryHaskellDepends = [
           aeson base byteable bytestring cryptohash lens lens-aeson mime
